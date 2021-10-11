dc235863f814b40b3241bbb6cd401d97692d3e4c
Luis BERNARDOS <luis.bernardos_barreda@onera.fr>
2021-10-11 09:38:25 +0200

    GAMMER extrusion enhancement GVD.extrude()

--- TEMPLATES/WORKFLOW_AIRFOIL/test-mesher.py
+++ TEMPLATES/WORKFLOW_AIRFOIL/test-mesher.py
@@ -2,7 +2,7 @@ import MOLA.WorkflowAirfoil as WF
 
 meshParams = WF.getMeshingParameters()
 meshParams['References']['DeltaYPlus'] = 0.5
-meshParams['References']['Reynolds']   = 333333.33
+meshParams['References']['Reynolds']   = 1e6
 
-WF.buildMesh('e562.dat', save_meshParams=False, save_mesh=True,
+WF.buildMesh('psu94097.txt', save_meshParams=False, save_mesh=True,
                          meshParams=meshParams,)
