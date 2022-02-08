dd0094d5b8bb75f3c1e0b25534a564943faa0da2
Mikel Balmaseda <mikel.balmaseda@onera.fr>
2022-02-08 14:16:46 +0100

    Rename Struct Module into Structure

--- env_MOLA.sh
+++ env_MOLA.sh
@@ -6,12 +6,12 @@ shopt -s expand_aliases
 
 ###############################################################################
 # ---------------- THESE LINES MUST BE ADAPTED BY DEVELOPERS ---------------- #
-export MOLA=/stck/lbernard/MOLA/Dev
-export TREELAB=/stck/lbernard/TreeLab/dev
-export EXTPYLIB=$MOLA/ExternalPythonPackages
-export MOLASATOR=/tmp_user/sator/lbernard/MOLA/Dev
+export MOLA=/stck/mbalmase/MOLA/Dev
+export TREELAB=/stck/mbalmase/TreeLab/dev
+export EXTPYLIB=/stck/lbernard/MOLA/Dev/ExternalPythonPackages
+export MOLASATOR=/tmp_user/sator/mbalmase/MOLA/Dev
 export TREELABSATOR=/tmp_user/sator/lbernard/TreeLab/dev
-export EXTPYLIBSATOR=$MOLASATOR/ExternalPythonPackages
+export EXTPYLIBSATOR=/tmp_user/sator/lbernard/MOLA/Dev/ExternalPythonPackages
 export PUMAVERSION=r337
 ###############################################################################
 
