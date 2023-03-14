e23bf91f98fd0eaef4b8e60534f8dca7820caf7b
Luis Bernardos <luis.bernardos@onera.fr>
2023-03-14 09:32:29 +0100

    included gamma-ReTheta quantities on injection for wf compressor

--- MOLA/WorkflowCompressor.py
+++ MOLA/WorkflowCompressor.py
@@ -2826,6 +2826,8 @@ def translateVariablesFromCGNS2Elsa(Variables):
         VelocityCorrelationYY    = 'inj_tur4', 
         VelocityCorrelationYZ    = 'inj_tur5', 
         VelocityCorrelationZZ    = 'inj_tur6',
+        Intermittency            = 'inj_tur3',
+        MomentumThicknessReynolds= 'inj_tur4',
     )
     if 'VelocityCorrelationXX' in Variables:
         # For RSM models
