onera/MOLA

commit c798bac17d226048bffcebdfb37b60534451fb07

Add flexibility for WorkflowCompressor.computeReferenceValues

MOLA/WorkflowCompressor.py
@@ -1125,18 +1125,13 @@ def getNumberOfBladesInMeshFromFamily(t, FamilyName, NumberOfBlades):
     return Nb
 
 def computeReferenceValues(FluidProperties, PressureStagnation,
-                           TemperatureStagnation, Surface, MassFlow=None, Mach=None, TurbulenceLevel=0.001,
-        Viscosity_EddyMolecularRatio=0.1, TurbulenceModel='Wilcox2006-klim',
-        TurbulenceCutoff=1e-8, TransitionMode=None, CoprocessOptions={},
-        Length=1.0, TorqueOrigin=[0., 0., 0.],
-        FieldsAdditionalExtractions=['ViscosityMolecular', 'Viscosity_EddyMolecularRatio', 'Pressure', 'Temperature', 'PressureStagnation', 'TemperatureStagnation', 'Mach', 'Entropy'],
-        BCExtractions=dict(
-            BCWall = ['normalvector', 'frictionvector','psta', 'bl_quantities_2d', 'yplusmeshsize'],
-            BCInflow = ['convflux_ro'],
-            BCOutflow = ['convflux_ro']),
-        AngleOfAttackDeg=0.,
-        YawAxis=[0.,0.,1.],
-        PitchAxis=[0.,1.,0.]):
+                           TemperatureStagnation, Surface, MassFlow=None, Mach=None,
+                           FieldsAdditionalExtractions=['ViscosityMolecular', 'Viscosity_EddyMolecularRatio', 'Pressure', 'Temperature', 'PressureStagnation', 'TemperatureStagnation', 'Mach', 'Entropy'],
+                           BCExtractions=dict(
+                             BCWall = ['normalvector', 'frictionvector','psta', 'bl_quantities_2d', 'yplusmeshsize'],
+                             BCInflow = ['convflux_ro'],
+                             BCOutflow = ['convflux_ro']),
+                            **kwargs):
     '''
     This function is the Compressor's equivalent of :func:`MOLA.Preprocess.computeReferenceValues`.
     The main difference is that in this case reference values are set through
@@ -1147,6 +1142,11 @@ def computeReferenceValues(FluidProperties, PressureStagnation,
 
     Please, refer to :func:`MOLA.Preprocess.computeReferenceValues` doc for more details.
     '''
+    ASSERTION_ERR = 'For this workflow, you must provide PressureStagnation, TemperatureStagnation and MassFlow (or Mach). '
+    ASSERTION_ERR+= 'You cannot provide Density, Temperature and Velocity'
+    assert all([not var in kwargs for var in ['Density', 'Temperature', 'Velocity']]), \
+        J.FAIL + ASSERTION_ERR + J.ENDC
+    
     # Fluid properties local shortcuts
     Gamma   = FluidProperties['Gamma']
     IdealGasConstant = FluidProperties['IdealGasConstant']
@@ -1181,6 +1181,8 @@ def computeReferenceValues(FluidProperties, PressureStagnation,
     TurboStatistics = ['rsd-{}'.format(var) for var in ['MassFlowIn', 'MassFlowOut',
         'PressureStagnationRatio', 'TemperatureStagnationRatio', 'EfficiencyIsentropic',
         'PressureStagnationLossCoeff']]
+    
+    CoprocessOptions = kwargs.pop('CoprocessOptions')
     try:
         RequestedStatistics = CoprocessOptions['RequestedStatistics']
         for stat in TurboStatistics:
@@ -1195,29 +1197,19 @@ def computeReferenceValues(FluidProperties, PressureStagnation,
         Density=Density,
         Velocity=Velocity,
         Temperature=Temperature,
-        AngleOfAttackDeg=AngleOfAttackDeg,
-        AngleOfSlipDeg = 0.0,
-        YawAxis=YawAxis,
-        PitchAxis=PitchAxis,
-        TurbulenceLevel=TurbulenceLevel,
         Surface=Surface,
-        Length=Length,
-        TorqueOrigin=TorqueOrigin,
-        TurbulenceModel=TurbulenceModel,
-        Viscosity_EddyMolecularRatio=Viscosity_EddyMolecularRatio,
-        TurbulenceCutoff=TurbulenceCutoff,
-        TransitionMode=TransitionMode,
         CoprocessOptions=CoprocessOptions,
         FieldsAdditionalExtractions=FieldsAdditionalExtractions,
-        BCExtractions=BCExtractions)
+        BCExtractions=BCExtractions,
+        **kwargs)
 
-    addKeys = dict(
-        PressureStagnation = PressureStagnation,
-        TemperatureStagnation = TemperatureStagnation,
-        MassFlow = MassFlow,
+    ReferenceValues.update(
+        dict(
+            PressureStagnation    = PressureStagnation,
+            TemperatureStagnation = TemperatureStagnation,
+            MassFlow              = MassFlow,
         )
-
-    ReferenceValues.update(addKeys)
+    )
 
     return ReferenceValues
 

commit b9e288cc8d03ce761ef48f76a5b70f45da9b17c4

Fix a major bug on WorkflowCompressor.computeReferenceValues introduced by commit c798bac1

MOLA/WorkflowCompressor.py
@@ -1150,6 +1150,7 @@ def getNumberOfBladesInMeshFromFamily(t, FamilyName, NumberOfBlades):
 
 def computeReferenceValues(FluidProperties, PressureStagnation,
                            TemperatureStagnation, Surface, MassFlow=None, Mach=None,
+                           YawAxis=[0.,0.,1.], PitchAxis=[0.,1.,0.],
                            FieldsAdditionalExtractions=['ViscosityMolecular', 'Viscosity_EddyMolecularRatio', 'Pressure', 'Temperature', 'PressureStagnation', 'TemperatureStagnation', 'Mach', 'Entropy'],
                            BCExtractions=dict(
                              BCWall = ['normalvector', 'frictionvector','psta', 'bl_quantities_2d', 'yplusmeshsize'],
@@ -1225,6 +1226,8 @@ def computeReferenceValues(FluidProperties, PressureStagnation,
         CoprocessOptions=CoprocessOptions,
         FieldsAdditionalExtractions=FieldsAdditionalExtractions,
         BCExtractions=BCExtractions,
+        YawAxis=YawAxis,
+        PitchAxis=PitchAxis,
         **kwargs)
 
     ReferenceValues.update(