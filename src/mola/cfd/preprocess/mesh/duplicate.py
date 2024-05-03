4dc9be92f84483e8e797762c75ca60b845b632ff
Thomas BONTEMPS <thomas.bontemps@onera.fr>
2024-05-03 17:45:46 +0200

    test new maia functions for duplication

--- src/mola/cfd/preprocess/mesh/duplicate.py
+++ src/mola/cfd/preprocess/mesh/duplicate.py
@@ -219,10 +219,22 @@ def duplicate_workflow_with_maia(workflow):
 @MaiaParallel
 def duplicate_with_maia(dist_tree, duplication_parameters, merge_zones=False):
     import maia
-    import maia.pytree as PT
     from mpi4py import MPI
     comm = MPI.COMM_WORLD
 
+    # TODO use the following lines when the env uses Maia v>1.3
+    # for row, dup_params in duplication_parameters.items():
+    #     if dup_params['number_of_duplications'] == 0:
+    #         continue
+    #     elif dup_params['is_360']:
+    #         mola_logger.info(f"  > row {row} is replicated on 360 degrees")
+    #         maia.algo.dist.duplicate_family_from_rotation_jns_to_360(dist_tree, row, comm)
+    #     else:
+    #         plurial = 's' if dup_params['number_of_duplications'] > 1 else ''
+    #         mola_logger.info(f"  > row {row} is replicated {dup_params['number_of_duplications']} time"+plurial)
+    #         maia.algo.dist.duplicate_family_from_periodic_jns(dist_tree, row, dup_params['number_of_duplications'], comm)
+        
+    import maia.pytree as PT
     for row, dup_params in duplication_parameters.items():
         if dup_params['number_of_duplications'] == 0:
             continue
@@ -252,7 +264,7 @@ def duplicate_with_maia(dist_tree, duplication_parameters, merge_zones=False):
                 comm, 
                 apply_to_fields=True  # TODO that becomes the default value with maia v1.4
                 )
-        
+    
     if merge_zones:
         maia.algo.dist.merge_connected_zones(dist_tree, comm)    
 
