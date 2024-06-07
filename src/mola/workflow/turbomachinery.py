1274fc6cf9048ac1717e37cebaacb216fda45eab
Thomas BONTEMPS <thomas.bontemps@onera.fr>
2024-06-07 17:16:05 +0200

    fix default extractions at BC for workflows turbomachinery and linear_cascade

--- src/mola/workflow/turbomachinery.py
+++ src/mola/workflow/turbomachinery.py
@@ -32,13 +32,9 @@ class WorkflowTurbomachinery(WorkflowRotatingComponent):
         if tree is not None:
             self.get_workflow_parameters_from_tree()
         else:
-            self.Extractions.extend([
-                dict(Type='BC', Source='BCWall*', Fields=['Pressure',
-                                                          'BoundaryLayer',
-                                                          'yPlus']),
-                dict(Type='BC', Source='BCInflow*', Fields=['MassFlow']),
-                dict(Type='BC', Source='BCOutflow*', Fields=['MassFlow']),
-            ])
+            self._interface.add_to_Extractions_BC(Source='BCWall*', Fields=['Pressure', 'BoundaryLayer', 'yPlus'])
+            self._interface.add_to_Extractions_BC(Source='BCInflow*', Fields=['MassFlow'])
+            self._interface.add_to_Extractions_BC(Source='BCOutflow*', Fields=['MassFlow'])
 
     def submit_iso_speed_line(self, ThrottleValues, ParallelMode=False, initialize_from_previous=True):
 
