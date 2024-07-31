e69d02a3bdc5fb5d0dd0bac0113c001eab28f917
Luis Bernardos <luis.bernardos@onera.fr>
2024-07-31 10:58:24 +0200

    solved conflict in network.py and when using juno

--- src/mola/env/onera/network.py
+++ src/mola/env/onera/network.py
@@ -1,19 +1,23 @@
+########## CAUTION IMPORTANT NOTE !!!!! 
+## COMMENTS INSIDE THE DICTIONNARIES ARE FORBIDEN IN THIS FILE
+
 AvailableEnvironments = ['ld', 'spiro', 'sator', 'juno']
 
+# NOTE THE COMMENTS ARE OUTSIDE OF DICT (OUTSIDE OF BRACKETS {})
+#   'n*' : 'sator',
+#   computation nodes on juno
+#   '^n0([0-2][0-9]|30|31|32)$' : 'juno',  # n[001-032]
 PatternsToEnvironments = {
     'ld*' : 'ld',
     'visung*' : 'ld',
     'spiro*' : 'spiro',
     'sator*' : 'sator',
-    # 'n*' : 'sator',
     'n03[3-9]' : 'sator',
     'n0[4-9][0-9]' : 'sator',
     'n1??' : 'sator',
     'n????' : 'sator',
     'f0[1-4]' : 'juno',
     'v00[1-6]' : 'juno',  
-    # computation nodes on juno
-    # '^n0([0-2][0-9]|30|31|32)$' : 'juno',  # n[001-032]
     'n00[1-9]' : 'juno',  
     'n01[0-9]' : 'juno',  
     'n02[0-9]' : 'juno',  
