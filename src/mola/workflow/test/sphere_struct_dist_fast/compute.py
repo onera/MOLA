0ec8e54b4aab5a88ea026d64e9cd715e1f51e47f
Luis Bernardos <luis.bernardos@onera.fr>
2024-08-26 10:11:58 +0200

    removed spurious files

--- src/mola/workflow/test/sphere_struct_dist_fast/compute.py
+++
@@ -1,4 +0,0 @@
-from mola.workflow import read_workflow
-import mola.naming_conventions as names
-workflow = read_workflow(names.FILE_INPUT_SOLVER)
-workflow.compute()
--- src/mola/workflow/test/sphere_struct_dist_fast/job.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /stck/lbernard/MOLA/Dev/src/mola/env/onera/ld/fast.sh
-unset "${!OMPI_@}" "${!MPI_@}"
-
-export KMP_WARNINGS=FALSE
-export OMP_PLACES=cores
-kpython -n 1 -t 8 compute.py 1>stdout.log 2>stderr.log
