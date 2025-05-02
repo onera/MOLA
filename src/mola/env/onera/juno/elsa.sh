6bdc05fa56709a85eead07c20b4bde078f12c6f2
Thomas BONTEMPS <thomas.bontemps@onera.fr>
2025-05-02 12:06:54 +0200

    update elsA to v5.4.01 and move radius repartition for outradeqhyb and stage_mxpl_hy b in the CGNS

--- src/mola/env/onera/juno/elsa.sh
+++ src/mola/env/onera/juno/elsa.sh
@@ -39,9 +39,9 @@ export FI_PROVIDER=tcp
 
 
 
-# maia
-module use --append /tmp_user/juno/sonics/usr/modules/
-module load maia/$MAIAVERSION-dsi-cfd6
+# # maia
+# module use --append /tmp_user/juno/sonics/usr/modules/
+# module load maia/$MAIAVERSION-dsi-cfd6
 
 # VPM
 export VPMPATH=/tmp_user/juno/lbernard/VPM/$VPMVERSION/juno_elsA/$ARCH
