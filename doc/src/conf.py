66419adde8500fbbc75dc619cba5a331e84afa8f
Thomas BONTEMPS <thomas.bontemps@onera.fr>
2025-11-20 10:02:07 +0100

    update doc theme

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -1,69 +1,136 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+from pathlib import Path
+import sys
 from mola import __version__
 
-import pathlib
-import sys
+THIS_DIR = Path(__file__).resolve().parent
+sys.path.insert(0, str(THIS_DIR))
 
-## Embedded onera_sphinx_theme
-# docs 'source' directory
-THIS_DIR = pathlib.Path(__file__).resolve().parent
-sys.path.insert(0,str(THIS_DIR.parent/"theme"))
-from onera_sphinx_theme import prepare,OST_THEME_DIR
+# -- Project information -----------------------------------------------------
 
-meta_data = {
-    "project": "MOLA",
-    "copyright": "ONERA",
-    "authors":["Luis Bernardos", "Thomas Bontemps"],
-    "html_static_path":["_static"],
-    "html_logo":"_static/icons/mola_logo.png",  # it also possible to define html_logo_dark and html_logo_light
-    "html_favicon": "_static/icons/mola_icon.ico",
-    "html_title":"MOLA documentation",
-    # "html_logo_text":"MOLA",
-    "substitutions": {'authors':'__authors__'},
-    "version": __version__,
+project = 'MOLA'
+copyright = 'ONERA'
+authors = ["Luis Bernardos", "Thomas Bontemps"],
+version = __version__
+stable_release = 'v1.19'
+# available versions are to be written in _static/versions.json
 
-    # "build_version_switcher": True,
-    # "all_versions": [__version__, "1.19"],
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autosectionlabel',
+    'sphinx_copybutton',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.autodoc', 
+    'sphinx.ext.graphviz',
+    'sphinx_design',
+    'mola_extension_for_sphinx',
+    ]
+
+suppress_warnings = ['autosectionlabel.*'] # to get rid of duplicate label warning
+
+intersphinx_mapping = {
+    'python':('https://docs.python.org/3',None),
+    'numpy':('https://numpy.org/doc/stable',None),
+    'scipy': ('https://docs.scipy.org/doc/scipy/', None),
+    'mpi4py': ('https://mpi4py.readthedocs.io/en/stable/', None),
+    'matplotlib': ('https://matplotlib.org/stable/', None),
+    'maia': ("https://onera.github.io/Maia/1.5/",None),
+    'Converter': ('http://elsa.onera.fr/Cassiopee/', '/stck/benoit/Cassiopee/Apps/Modules/Converter/doc/build/objects.inv'),
+    }
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
-    "docsbuilddir":"../html",
-    
-    "header_navbar_links":{ #list of {"name":...,"target":...} dicts or dict <name:target>
-        "User manual":  "user_manual/index",
-        "Examples": "examples/index",
-        "Tutorials": "tutorials/index",
-        "Developer manual": "developer_manual/index",
-        "Changelog" : "changelog",
-    },
-    # "header_navbar_external_links":[
-    #     {"url":"https://www.google.com","name":"Google"}
-    # ],
-    "header_links_before_dropdown":6,
-    "header_right_icons":[
-        {'url':'https://gitlab.onera.net/numerics/mola', 'icon':'gitlab', 'name':'MOLA GitLab'},
-        {'url':'https://github.com/onera/MOLA', 'icon':'_static/icons/github-white.svg', 'name':'MOLA GitHub'},
-        {'url':'https://github.com/Luispain/treelab', 'icon':'_static/icons/treelab.png', 'name':'Treelab GitHub'},
-        {'url':'https://numerics.gitlab-pages.onera.net/mola/v1.19', 'icon':'_static/icons/mola_v1.png', 'name':'MOLA v1'},
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'pydata_sphinx_theme'
+html_style = '_static/css/custom.css'
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+html_theme_options = {
+    "navbar_start": ["navbar-logo"], #"navbar-version"],
+    "navbar_align": "content",
+    "header_links_before_dropdown": 5,
+    'navbar_end': ['theme-switcher', 'navbar-icon-links', 'version-switcher'],
+    "switcher": {
+        "json_url": "https://numerics.gitlab-pages.onera.net/mola/Dev/_static/versions.json",
+        "version_match": stable_release,
+        },
+
+    "navbar_persistent": ["search-button"],
+
+    "icon_links": [
+
+        {
+            'name':'MOLA GitLab',
+            'url':'https://gitlab.onera.net/numerics/mola', 
+            # 'icon':'_static/icons/gitlab.svg', 
+            "icon": "fa-brands fa-gitlab",
+            },
+        {
+            'name':'MOLA GitHub',
+            'url':'https://github.com/onera/MOLA', 
+            # 'icon':'_static/icons/github-white.svg', 
+            "icon": "fa-brands fa-github",
+            },
+        {
+            'name':'Treelab GitHub',
+            'url':'https://github.com/Luispain/treelab', 
+            'icon':'_static/icons/treelab.png', 
+            'type': 'local'
+            },
+        {
+            'name':'MOLA v1',
+            'url':'https://numerics.gitlab-pages.onera.net/mola/v1.19', 
+            'icon':'_static/icons/mola_v1.png', 
+            'type': 'local',
+            },
+
     ],
-    "intersphinx_mapping":{
-        'python':('https://docs.python.org/3',None),
-        'numpy':('https://numpy.org/doc/stable',None),
-        'scipy': ('https://docs.scipy.org/doc/scipy/', None),
-        'mpi4py': ('https://mpi4py.readthedocs.io/en/stable/', None),
-        'matplotlib': ('https://matplotlib.org/stable/', None),
-        'maia': ("https://onera.github.io/Maia/1.5/",None),
-        'Converter': ('http://elsa.onera.fr/Cassiopee/', '/stck/benoit/Cassiopee/Apps/Modules/Converter/doc/build/objects.inv'),
-    },
 }
 
-prepare(
-    globals(),
-    meta_data, # json, yaml or dict
-)
-# Add other extensions not already loaded with the theme.
-# Variables are accessible through globals()
-extensions.add('mola_extension_for_sphinx')
-extensions.add('sphinx.ext.graphviz')
+html_logo = str(Path('_static/icons/mola_logo.png'))
+html_favicon = str(Path('_static/icons/mola_icon.ico'))
 
-# remove warnings about duplicate labels
-# see https://stackoverflow.com/a/77577337
-suppress_warnings = ['autosectionlabel.*'] 
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+numfig = True
+todo_include_todos = True
+autodoc_member_order = 'bysource'
+
+rst_epilog = f"""
+.. |version| replace:: {__version__}
+.. |release| replace:: {__version__}
+"""
