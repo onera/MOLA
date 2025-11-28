fixing Torque sign in propeller signal

src/mola/cfd/postprocess/signals/propeller_coefficients_computer.py
@@ -73,11 +73,11 @@ def _update_torque_coefficients(coefs : dict, tx, ty, tz, ApplicationContext : d
     if np.linalg.norm(rotation_center) != 0:
         raise NotImplementedError("rotatation center must be (0,0,0)")
 
-    RPS = np.abs(getRPS(ApplicationContext))
+    RPS = getRPS(ApplicationContext)
     RPM = 60 * RPS
 
-    Torque = fields.project_load(tx,ty,tz,-axis)
-    Power = Torque * RPM * np.pi/30
+    Torque = fields.project_load(tx,ty,tz,np.sign(RPS)*axis)
+    Power = Torque * np.abs(RPM) * np.pi/30
 
     Thrust = coefs['Thrust']
     CT = coefs['CT']