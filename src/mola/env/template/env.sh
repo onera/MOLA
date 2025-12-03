eea13a6ec6b0104ed097980d31ba2a9f9b2d028c
Thomas BONTEMPS <thomas.bontemps@onera.fr>
2025-12-03 15:53:07 +0100

    fix env

--- src/mola/env/template/env.sh
+++ src/mola/env/template/env.sh
@@ -19,7 +19,11 @@
 SCRIPT_DIR=$( \cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export MOLA_MACHINE=$(python3 $SCRIPT_DIR/network.py)
 
-export MOLA_SOLVER=elsa  # you may choose your default solver here
+if [ "$1" = "" ]; then
+    export MOLA_SOLVER=elsa  # you may choose your default solver here
+else
+    export MOLA_SOLVER=$1
+fi
 
 source $SCRIPT_DIR/network.sh
 
